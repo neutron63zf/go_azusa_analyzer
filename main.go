c3946c491579da21d971456fbffec835620ae0cf
Nkowne63 <neutron63zf@gmail.com>
2018-07-19 18:11:32 +0900

    completed: first

--- main.go
+++ main.go
@@ -2,28 +2,103 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"sync"
+	"time"
 
 	"./getdoc"
 	"./parsedoc"
 )
 
-func printSR(mid int) {
-	doc, err := getdoc.GetDocumentByMid(mid)
-	if err != nil {
-		fmt.Println("!getdoc.GetDocumentByMid ->")
-		fmt.Println(err)
-		return
+type mail struct {
+	mid       int
+	sender    string
+	recievers []string
+	isValid   bool
+}
+
+func getMailByMid(mid int) (mail, error) {
+	var m mail
+	for {
+		// log.Printf("try loading %v...\n", mid)
+		doc, err := getdoc.GetDocumentByMid(mid)
+		if err != nil {
+			log.Println("!getdoc.GetDocumentByMid ->")
+			log.Println(err)
+			// retry
+			// return m, err
+			time.Sleep(3 * time.Second)
+			continue
+		}
+		if !parsedoc.IsNotDenialMessage(doc) || !parsedoc.IsValidMid(doc) || !parsedoc.IsTheMidExists(doc) {
+			log.Println("!parsedoc, document not valid")
+			m = mail{
+				isValid: false,
+			}
+			return m, nil
+		}
+		sender, recievers, err := parsedoc.GetSenderAndRecievers(doc)
+		if err != nil {
+			log.Println("!parsedoc.GetSenderAndRecievers ->")
+			log.Println(err)
+			// retry
+			// return m, err
+			continue
+		}
+		m = mail{
+			mid:       mid,
+			sender:    sender,
+			recievers: recievers,
+			isValid:   true,
+		}
+		if len(m.sender) == 0 {
+			// 送信者がいないのは流石におかしい
+			// retry
+			continue
+		}
+		break
 	}
-	sender, recievers, err := parsedoc.GetSenderAndRecievers(doc)
-	fmt.Println(mid)
-	fmt.Println(sender)
-	fmt.Println(recievers)
-	fmt.Println()
+	return m, nil
+}
+
+func convergeDataByMidBetween(startMid int, endMid int) chan []mail {
+	var wg sync.WaitGroup
+	var err error
+	remains := 0
+	allcount := endMid - startMid + 1
+	de := make([]mail, allcount, allcount)
+	for i := startMid; i <= endMid; i++ {
+		wg.Add(1)
+		remains++
+		go func(mid int) {
+			defer func() {
+				wg.Done()
+				remains--
+				log.Printf("mid %v ended. %v / %v remains\n", mid, remains, allcount)
+			}()
+			de[mid-startMid], err = getMailByMid(mid)
+			if err != nil {
+				log.Println("!getMailByMid ->")
+				log.Println(err)
+				de[mid-startMid] = mail{mid: mid}
+			}
+		}(i)
+	}
+	c := make(chan []mail)
+	go func() {
+		defer close(c)
+		wg.Wait()
+		c <- de
+		return
+	}()
+	return c
 }
 
 func main() {
-	for i := 622000; i < 622600; i++ {}
-		// go をつけると、並列先のが終わる前にメインのスレッドが終わってしまい、何も出ない
-		printSR(i)
+	c := convergeDataByMidBetween(603657, 622345)
+	log.Println("procedure start")
+	arr := <-c
+	for m := range arr {
+		fmt.Println(arr[m])
 	}
 }
--- parsedoc/parsedoc.go
+++ parsedoc/parsedoc.go
@@ -8,12 +8,26 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-// IsDenialMessage returns whether doc is reject message
-func IsDenialMessage(doc *goquery.Document) bool {
+// IsNotDenialMessage returns whether doc is reject message
+func IsNotDenialMessage(doc *goquery.Document) bool {
 	text := doc.Find("body").Text()
 	r1 := regexp.MustCompile(`不正な操作が行われたか、cookieが有効になっていません。`)
 	r2 := regexp.MustCompile(`ログイン操作をやり直してください。`)
-	return r1.MatchString(text) && r2.MatchString(text)
+	return !(r1.MatchString(text) && r2.MatchString(text))
+}
+
+// IsValidMid returns whether the mid is valid
+func IsValidMid(doc *goquery.Document) bool {
+	text := doc.Find("body").Text()
+	r := regexp.MustCompile(`メッセージIDが不正です`)
+	return !r.MatchString(text)
+}
+
+// IsTheMidExists returns whether the log exists
+func IsTheMidExists(doc *goquery.Document) bool {
+	text := doc.Find("body").Text()
+	r := regexp.MustCompile(`そのログはありません`)
+	return !r.MatchString(text)
 }
 
 // GetTimeStr returns time string of message
@@ -21,7 +35,7 @@ func GetTimeStr(doc *goquery.Document) string {
 	return doc.Find(".azusa-log i").Text()
 }
 
-// GetSenderAndReciever returns sender recievers slice
+// GetSenderAndRecievers returns sender recievers slice
 func GetSenderAndRecievers(doc *goquery.Document) (string, []string, error) {
 	// azusaのメッセージの部分のマークアップは存在しないため、正規表現しかない
 	html, err := doc.Find(".azusa-log").Html()
